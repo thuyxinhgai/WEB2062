a0acf6d4e3df0761ac0810139cedf21ecafeba96
Thuy <thuyntqph27836@fpt.edu.vn>
2023-01-29 23:32:14 +0700

    thuy code

--- bai 3.js
+++ bai 3.js
@@ -35,31 +35,77 @@ const restaurent = {
         console.log(othersIngredients);
     },
 };
-const rest1 ={
-    name: 'Capri',
-    // numGuests: 20.
-    numGuests: 0.
-};
-const rest2 ={
-    name: 'La Pizza',
-    owner: 'Giovanni Rossi',
-};
-// or
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
+const game ={
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer','Pavard','Martinez','Alaba','Davies','Kimmich','Goretzka','Coman','Muller','Gnarby','Lewandowski',
+        ],
+        [
+            'Burki','Schulz','Hummels','Akanji','Hakimi','Weigl','Witsel','Hazard','Brandt','Sancho','Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date : 'Nov 9th, 2037',
+    odds:{
+        team1:1.33,
+        x : 3.23,
+        team2:6.5,
 
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-//nullish
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
-// AND
-// rest1.owner = rest1.owner && '<ANONYMONUS>';
-// rest2.owner = rest2.owner && '<ANONYMONUS>';
-rest1.owner &&= '<ANONYMONUS>';
-rest2.owner &&= '<ANONYMONUS>';
-console.log(rest1);
-console.log(rest2);
+    },
+};
+//1
+const[ players1, players2] = game.players;
+console.log(players1, players2);
+//2
+const[ gk, ...fieldPlayers] = players1;
+console.log( gk,fieldPlayers);
+//3
+const allPlayers = [...players1,...players2];
+console.log(allPlayers);
+//4
+const playersFinal = [...players1,'Thiago','Coutinho','Periscic'];
+//5
+const {odds: {team1, x= draw, team2}} = game;
+console.log(team1, draw, team2);
+//6
+const printGoals = function(...players){
+    console.log(players);
+    console.log(`$(players.length) goals were scored`)
+}
+// printGoals('Davies','Muller','Lewandowski','kimmich');
+// printGoals('Davies','Muller');
+printGoals(...game.scored);
+//7
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 1 is more likely to win');
+// const rest1 ={
+//     name: 'Capri',
+//     // numGuests: 20.
+//     numGuests: 0.
+// };
+// const rest2 ={
+//     name: 'La Pizza',
+//     owner: 'Giovanni Rossi',
+// };
+// // or
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+
+// // rest1.numGuests ||= 10;
+// // rest2.numGuests ||= 10;
+// //nullish
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+// // AND
+// // rest1.owner = rest1.owner && '<ANONYMONUS>';
+// // rest2.owner = rest2.owner && '<ANONYMONUS>';
+// rest1.owner &&= '<ANONYMONUS>';
+// rest2.owner &&= '<ANONYMONUS>';
+// console.log(rest1);
+// console.log(rest2);
 // restaurent.numGuests = 0;
 // const guests = restaurent.numGuests || 10;
 // console.log(guests);
