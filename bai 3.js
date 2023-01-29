f62b8bd23b550f59b4fc573b11193314c3fc7fe7
Thuy <thuyntqph27836@fpt.edu.vn>
2023-01-29 21:30:41 +0700

    array

--- bai 3.js
+++ bai 3.js
@@ -35,12 +35,37 @@ const restaurent = {
         console.log(othersIngredients);
     },
 };
-restaurent.numGuests = 0;
-const guests = restaurent.numGuests || 10;
-console.log(guests);
-//
-const guestCorrect= restaurent.numGuests ?? 10;
-console.log(guestCorrect);
+const rest1 ={
+    name: 'Capri',
+    // numGuests: 20.
+    numGuests: 0.
+};
+const rest2 ={
+    name: 'La Pizza',
+    owner: 'Giovanni Rossi',
+};
+// or
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+//nullish
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+// AND
+// rest1.owner = rest1.owner && '<ANONYMONUS>';
+// rest2.owner = rest2.owner && '<ANONYMONUS>';
+rest1.owner &&= '<ANONYMONUS>';
+rest2.owner &&= '<ANONYMONUS>';
+console.log(rest1);
+console.log(rest2);
+// restaurent.numGuests = 0;
+// const guests = restaurent.numGuests || 10;
+// console.log(guests);
+// //
+// const guestCorrect= restaurent.numGuests ?? 10;
+// console.log(guestCorrect);
 // console.log('---- OR ----');
 // console.log(3 || 'Jonas');
 // console.log('' || 'Jonas');
